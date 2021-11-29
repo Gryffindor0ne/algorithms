f9477a88be715ad2024c334cf283287a83b0b94b
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2021-11-29 21:51:40 +0900

    로또의 최고순위와 최저 순위

---
+++ programmers/algorithms1.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// https://programmers.co.kr/learn/courses/30/lessons/77484
+
+function solution(lottos, win_nums) {
+  let answer = [];
+  let matched = 0;
+  let zeroCheck = 0;
+  for (let i = 0; i < lottos.length; i++) {
+    if (lottos[i] === 0) {
+      zeroCheck++;
+    } else if (win_nums.includes(lottos[i])) {
+      matched++;
+    }
+    answer = [
+      matched + zeroCheck > 1 ? 7 - (matched + zeroCheck) : 6,
+      matched > 1 ? 7 - matched : 6,
+    ];
+  }
+  return answer;
+}
