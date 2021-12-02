e0545965cc5d3a93eaf5a54cbb9ae78a37e03aa6
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2021-12-02 13:28:12 +0900

    음양 더하기

---
+++ programmers/algorithms4.js
@@ -0,0 +1,18 @@
+"use strict";
+// https://programmers.co.kr/learn/courses/30/lessons/76501
+
+function solution(absolutes, signs) {
+  const newArr = absolutes.map((el, idx) => {
+    if (signs[idx] === false) {
+      return el * -1;
+    }
+    return el;
+  });
+
+  const result = newArr.reduce((acc, cur) => acc + cur);
+
+  return result;
+}
+
+console.log(solution([4, 7, 12], [true, false, true]));
+console.log(solution([1, 2, 3], [false, false, true]));
