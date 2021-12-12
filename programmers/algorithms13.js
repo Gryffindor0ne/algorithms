194b1ef40e02610bedac60091073c8d731e3f2dd
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2021-12-12 16:08:14 +0900

    예산

---
+++ programmers/algorithms13.js
@@ -0,0 +1,29 @@
+/*
+
+예산
+https://programmers.co.kr/learn/courses/30/lessons/12982
+
+*/
+
+function solution(d, budget) {
+  let count = 0;
+  let sum = 0;
+  d.sort((a, b) => a - b);
+
+  let total = d.reduce((a, c) => a + c);
+
+  if (total > budget) {
+    for (let i = 0; i < d.length; i++) {
+      sum += d[i];
+      if (sum <= budget) {
+        count++;
+      } else {
+        return count;
+      }
+    }
+  }
+  return d.length;
+}
+
+console.log(solution([1, 3, 2, 5, 4], 9));
+console.log(solution([2, 2, 3, 3], 10));
