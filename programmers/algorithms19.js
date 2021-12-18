335d660fb7602dfc930824ede8047a3008c609b8
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2021-12-18 10:42:55 +0900

    소수찾기

---
+++ programmers/algorithms19.js
@@ -0,0 +1,24 @@
+/*
+
+소수찾기
+https://programmers.co.kr/learn/courses/30/lessons/12921
+
+*/
+
+function solution(n) {
+  let primes = [0, 0];
+
+  for (let i = 2; i <= n; i++) {
+    primes[i] = i;
+  }
+
+  for (let i = 2; i <= n; i++) {
+    if (primes[i] === 0) continue;
+    for (let j = i * 2; j <= n; j += i) {
+      primes[j] = 0;
+    }
+  }
+  return primes.filter((e) => e !== 0).length;
+}
+
+console.log(solution(10));
