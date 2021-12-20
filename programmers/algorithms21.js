2449d6bab33e0925e497d64e9b6ee64155e03514
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2021-12-20 12:43:20 +0900

    시저 암호

---
+++ programmers/algorithms21.js
@@ -0,0 +1,34 @@
+/*
+
+시저 암호
+https://programmers.co.kr/learn/courses/30/lessons/12926
+
+*/
+
+function solution(s, n) {
+  const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const alpha2 = "abcdefghijklmnopqrstuvwxyz";
+  let answer = "";
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === " ") {
+      answer += " ";
+    } else if (alpha.indexOf(s[i]) < 0) {
+      const findIdx = alpha2.indexOf(s[i]);
+      const check = findIdx + n;
+      const idx = check > 25 ? check - 26 : check;
+
+      answer += alpha2[idx];
+    } else {
+      const findIdx = alpha.indexOf(s[i]);
+      const check = findIdx + n;
+      const idx = check > 25 ? check - 26 : check;
+
+      answer += alpha[idx];
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution("a B z", 4));
