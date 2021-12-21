915e0ecca15fa53aef3b4a997068948065d925f5
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2021-12-21 09:29:52 +0900

    이상한 문자 만들기

---
+++ programmers/algorithms22.js
@@ -0,0 +1,28 @@
+/*
+
+이상한 문자 만들기
+https://programmers.co.kr/learn/courses/30/lessons/12930
+
+*/
+
+function solution(s) {
+  let result = [];
+
+  const changeWord = (word) => {
+    let newWord = "";
+    for (let i = 0; i < word.length; i++) {
+      i % 2 === 0
+        ? (newWord += word[i].toUpperCase())
+        : (newWord += word[i].toLowerCase());
+    }
+    return newWord;
+  };
+
+  s.split(" ").forEach((el) => {
+    result.push(changeWord(el));
+  });
+
+  return result.join(" ");
+}
+
+console.log(solution("try hello world"));
