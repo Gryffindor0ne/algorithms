ab11926c5c566f11fa10e54d03692c888a0e707e
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2021-12-23 12:34:52 +0900

    폰켓몬

--- programmers/Level1/algorithms12.js
+++ programmers/Level1/algorithms12.js
@@ -13,6 +13,7 @@ function solution(nums) {
   return choiceNum > newNums.length ? newNums.length : choiceNum;
 }
 
+
 console.log(solution([3, 1, 2, 3]));
 console.log(solution([3, 3, 3, 2, 2, 4]));
 console.log(solution([3, 3, 3, 2, 2, 2, 6, 6, 6]));
