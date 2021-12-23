941c9103e391edb11b324e607eb07f6f1924a12c
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2021-12-23 12:25:36 +0900

    크레인 인형뽑기 게임 🪅

--- programmers/Level1/algorithms3.js
+++ programmers/Level1/algorithms3.js
@@ -1,6 +1,12 @@
 "use strict";
 
-// https://programmers.co.kr/learn/courses/30/lessons/64061
+/*
+
+크레인 인형뽑기 게임
+
+https://programmers.co.kr/learn/courses/30/lessons/64061
+
+*/
 
 function solution(board, moves) {
   let answer = 0;
